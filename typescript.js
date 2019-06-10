2554f6e554a3214261ba1d109e7d3de95b891cea
Matt Forster <forster.matth@gmail.com>
2019-06-10 12:27:48 -0600

    fix(typescript): add parser, ts recommended overrides

--- typescript.js
+++ typescript.js
@@ -1,17 +1,19 @@
 // Rules that apply to typescript projects
 
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
     '@autovance/eslint-config-autovance/common',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'prettier/@typescript-eslint'
   ],
-  parser: '@typescript-eslint/parser',
   rules: {
-    '@typescript-eslint/camelcase': false,
-    '@typescript-eslint/no-explicit-any': false,
-    '@typescript-eslint/no-parameter-properties': false,
-    '@typescript-eslint/explicit-function-return-type': false
+    'constructor-super': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off'
   }
 };
