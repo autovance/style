7c95aa5b4bdfe26a13fd7e7842a5a414562102b2
Tyler Stewart <tyler@autovance.com>
2020-01-17 12:39:38 -0700

    feat: disable typescript no-unused-vars
    Already handled by based no-unused-vars

--- typescript.js
+++ typescript.js
@@ -14,6 +14,7 @@ module.exports = {
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-parameter-properties': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off'
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off'
   }
 };
